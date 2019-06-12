425b81a92b0cd676322579913a1bad8d4344b896
Alex Vassiliou <alex@20diamond.com>
2019-06-12 15:07:53 +1000

    added init to router

--- lib/router.rb
+++ lib/router.rb
@@ -1,2 +1,12 @@
+require_relative 'tickets_controller'
+require_relative 'tickets_view'
+require_relative 'command'
+#this class provides all the routes for the app
 class Router
+  def initialize(controller)
+    @controller = controller
+    @view = TicketsView.new
+    @menu = PresentationOptions.new
+    @running = true
+  end
 end
