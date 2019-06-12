98ad9a5e1d45042ae7f8c74b27fc44c112c2ff53
Alex Vassiliou <alex@20diamond.com>
2019-06-12 14:58:48 +1000

    added user commands and spec

---
+++ lib/command.rb
@@ -0,0 +1,25 @@
+# process command to send to controller
+# NB due to time restriction I wasnt fully able to implement this pattern
+# currently it is just parsing the user input into a symbol
+class Command
+  def self.process(command)
+    case command.strip.upcase
+    when/\ANEXT\Z/
+      :next_page
+    when /\APREV\Z/
+      :prev_page
+    when /\AGOTO\Z/
+      :goto_page
+    when /\ALIST\Z/
+      :list
+    when /\AFIND\Z/
+      :find
+    when /\AMAIN\Z/
+      :main
+    when /\AEXIT\Z/
+      :exit
+    else
+      :invalid
+    end
+  end
+end
