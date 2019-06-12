cdd40ee1a5c40e36f7eb7bce226a86c72e1b65d1
Alex Vassiliou <alex@20diamond.com>
2019-06-12 15:04:59 +1000

    added menu options

---
+++ lib/presentation_options.rb
@@ -0,0 +1,46 @@
+#this is the diplay for all the different menu options
+class PresentationOptions
+  def main
+    [
+      ['---------------------------------'],
+      ['what would you like to do?'],
+      ['---------------------------------'],
+      ['type one of the following options:'],
+      ['   list  |    to view all tickets'],
+      ['   find  |    to view a ticket'],
+      ['   exit  |    to stop and exit']
+    ]
+  end
+
+  def index
+    [
+      [''],
+      ['options:'],
+      ['next | prev | goto | find | main | exit'],
+      ['']
+    ]
+  end
+
+  def show
+    [
+      [''],
+      ['options:'],
+      ['back | find | main | exit'],
+      ['']
+    ]
+  end
+
+  def welcome
+    [
+      [''],
+      ['########################################'],
+      ['Welcome to the Tickets Viewer!!!'],
+      ['########################################'],
+      ['']
+    ]
+  end
+
+  def invalid_input
+    ['INVALID INPUT']
+  end
+end
